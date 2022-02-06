54033ea2a2bcce7b5c6fd8cd933e5a59cdb5acf4
hakki <hyener@invio.com>
2022-02-06 19:12:17 +0300

    Endpoint class added.

---
+++ Ounass/ServiceManager/Endpoint.swift
@@ -0,0 +1,45 @@
+//
+//  Endpoint.swift
+//  Ounass
+//
+//  Created by Hakkı Yiğit Yener on 6.02.2022.
+//
+
+import Foundation
+
+/*
+enum NetworkEnvironment {
+    case production
+}
+*/
+public enum OunessApi {
+    case productList(page:Int)
+    case productDetail(sku:String)
+    
+    var environmentBaseURL: String {
+        return "https://www.ounass.ae/"
+    }
+    
+    var path: String {
+        switch self {
+        case .productList(_):
+            return "api/women/clothing"
+        case .productDetail(_):
+            return "product/findbyslug"
+        }
+    }
+    
+    var requestURL: String {
+        return environmentBaseURL + path
+    }
+    
+    
+    var params: [String:Encodable] {
+        switch self {
+        case .productList(let page):
+            return ["p":page]
+        case .productDetail(let sku):
+            return ["slug":sku]
+        }
+    }
+}
--- Ounass/ServiceManager/ProductList/ProductListService.swift
+++ Ounass/ServiceManager/ProductList/ProductListService.swift
@@ -10,8 +10,8 @@ import Alamofire
 
 class ProductListService: ProductListServiceProtocol {
     func fetchProductList(page: Int, shouldClear: Bool, completionHandler: @escaping (ProductList?, Error?) -> ()) {
-        let params = ["p":page]
-        AF.request("https://www.ounass.ae/api/women/clothing", parameters: params).responseDecodable { (response:DataResponse<ProductList, AFError>) in
+        let requestVariables = OunessApi.productList(page: page)
+        AF.request(requestVariables.requestURL, parameters: requestVariables.params).responseDecodable { (response:DataResponse<ProductList, AFError>) in
             switch response.result {
             case .success(let productList):
                 completionHandler(productList, nil)
